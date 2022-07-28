4ff87d4e9a026f6287ce64436fa830f4ad6c0a5e
XuJiaobin <xiaojiuwo57@gmail.com>
2022-07-28 21:31:44 +0800

    更改element-ui版本，降级到2.15.8 Unix时间戳完成

--- src/pages/transcoding/tool-UnixTimeCoding.vue
+++ src/pages/transcoding/tool-UnixTimeCoding.vue
@@ -1,56 +1,119 @@
 <template>
-  <div>
-    <p class="showUnix"  v-cloak>
-      当前的Unix时间戳是：<span>{{ timeStamp }}</span>
-    </p>
-    <div class="unix2date">Unix时间戳（Unix timestamp）：
-      <el-input class="unix2dateFinal"/>
-      <el-select placeholder="请选择" v-model="selectVal">
-        <el-option value="秒"></el-option>
-        <el-option value="毫秒"></el-option>
-      </el-select>
-      <el-button type="primary">转换</el-button>
-      </div>
-  </div>
+    <div>
+        <p class="showUnix" v-cloak>
+            当前的Unix时间戳是：<span>{{ timeStamp }} </span>当前时间：<span>{{ currentTime }}</span>
+        </p>
+        <div class="unix2date">
+            Unix时间戳（Unix timestamp）：
+            <el-input class="unix2dateEnter" v-model="unix2dateEnter" />
+            <el-select placeholder="请选择" v-model="selectVal">
+                <el-option value="秒"></el-option>
+                <el-option value="毫秒"></el-option>
+            </el-select>
+            <el-button type="primary" @click="convertDateTime">转换</el-button>
+            <el-input class="unix2dateResult" readonly v-model="unix2dateResult" />
+        </div>
+        <div class="date2unix">
+            时间（年/月/日 时:分:秒）：
+            <el-date-picker v-model="date2unixEnter" :value="date2unixEnter" type="datetime" placeholder="选择日期时间" format="yyyy-MM-dd HH:mm:ss">
+            </el-date-picker>
+            <el-button type="primary" @click="convertTimeStamp">转换成Unix时间戳</el-button>
+            <el-input class="unix2dateResult" readonly v-model="date2unixResult" />
+            <el-select placeholder="请选择" v-model="date2UnixSelectVal">
+                <el-option value="秒"></el-option>
+                <el-option value="毫秒"></el-option>
+            </el-select>
+        </div>
+    </div>
 </template>
 
 <script>
-import dayjs from 'dayjs'
+import dayjs from "dayjs";
 export default {
-  name: "toolUnixTimeCoding",
-  data() {
-    return {
-      timeStamp: "",
-      selectVal:''
-    };
-  },
-  mounted() {
-    console.log(dayjs.unix(1659000285).format('YYYY-MM-DD HH:mm:ss'))
-    this.timeStamp = Math.round(new Date().getTime() / 1000);
-    setInterval(() => {
-      this.timeStamp = Math.round(new Date().getTime() / 1000);
-    }, 1000);
-  },
+    name: "toolUnixTimeCoding",
+    data() {
+        return {
+            timeStamp: "",
+            selectVal: "秒",
+            currentTime: "",
+            unix2dateEnter: "",
+            unix2dateResult: "",
+            date2UnixSelectVal: "秒",
+            date2unixEnter: "",
+            date2unixResult: "",
+        };
+    },
+    mounted() {
+        this.timeStamp = dayjs().unix();
+        this.currentTime = dayjs().format("YYYY年MM月DD日 HH:mm:ss");
+        setInterval(() => {
+            this.timeStamp = dayjs().unix();
+            this.currentTime = dayjs().format("YYYY年MM月DD日 HH:mm:ss");
+        }, 1000);
+    },
+    methods: {
+        convertDateTime() {
+            if (this.selectVal == "秒") {
+                this.unix2dateResult = dayjs.unix(parseInt(this.unix2dateEnter)).format("YYYY年MM月DD日 HH:mm:ss");
+            } else {
+                this.unix2dateResult = dayjs(parseInt(this.unix2dateEnter)).format("YYYY年MM月DD日 HH:mm:ss");
+            }
+        },
+        convertTimeStamp() {
+            if (this.date2UnixSelectVal == "秒") {
+                this.date2unixResult = dayjs(this.date2unixEnter).unix();
+            } else {
+                this.date2unixResult = dayjs(this.date2unixEnter).valueOf();
+            }
+        },
+    },
+    watch: {
+        date2UnixSelectVal(newVal) {
+            if (this.date2unixResult != "") {
+                if (newVal == "毫秒") {
+                    this.date2unixResult = dayjs(this.date2unixEnter).valueOf();
+                } else {
+                    this.date2unixResult = dayjs(this.date2unixEnter).unix();
+                }
+            }
+        },
+    },
 };
 </script>
 
 <style scoped>
 .showUnix {
-  text-align: center;
-  border-bottom: 1px solid #d7dae2;
-  padding-bottom: 20px;
+    text-align: center;
+    border-bottom: 1px solid #d7dae2;
+    padding-bottom: 20px;
 }
 .showUnix span {
-  color: #ff6200;
+    color: #ff6200;
 }
 [v-cloak] {
-  display: none;
+    display: none;
 }
-.unix2date{
-  padding-top: 20px;
+.unix2date {
+    padding-top: 20px;
 }
-.unix2dateFinal{
-  display: inline-block;
-  width: 20%;
+.unix2dateEnter {
+    display: inline-block;
+    width: 20%;
+}
+.el-select {
+    margin: 0 1%;
+    width: 6%;
+    display: inline-block;
+}
+.unix2dateResult {
+    width: 15%;
+    margin-left: 1%;
+}
+.date2unix {
+    margin-top: 1%;
+}
+.date2unix,
+.unix2date {
+    text-align: center;
 }
 </style>
--- src/plugin/element-ui-import.js
+++ src/plugin/element-ui-import.js
@@ -27,34 +27,36 @@ import {
     Option,
     Dialog,
     Popover,
+    DatePicker,
 } from "element-ui";
 export default {
     install() {
-        Vue.component(Dialog.name,Dialog);
-        Vue.component(Row.name,Row);
-        Vue.component(Input.name,Input);
-        Vue.component(Button.name,Button);
-        Vue.component(Dropdown.name,Dropdown);
-        Vue.component(DropdownMenu.name,DropdownMenu);
-        Vue.component(DropdownItem.name,DropdownItem);
-        Vue.component(Menu.name,Menu);
-        Vue.component(Submenu.name,Submenu);
-        Vue.component(MenuItem.name,MenuItem);
-        Vue.component(MenuItemGroup.name,MenuItemGroup);
-        Vue.component(Container.name,Container);
-        Vue.component(Header.name,Header);
-        Vue.component(Aside.name,Aside);
-        Vue.component(Main.name,Main);
-        Vue.component(Footer.name,Footer);
-        Vue.component(Switch.name,Switch);
-        Vue.component(Table.name,Table);
-        Vue.component(TableColumn.name,TableColumn);
-        Vue.component(Col.name,Col);
+        Vue.component(Dialog.name, Dialog);
+        Vue.component(Row.name, Row);
+        Vue.component(Input.name, Input);
+        Vue.component(Button.name, Button);
+        Vue.component(Dropdown.name, Dropdown);
+        Vue.component(DropdownMenu.name, DropdownMenu);
+        Vue.component(DropdownItem.name, DropdownItem);
+        Vue.component(Menu.name, Menu);
+        Vue.component(Submenu.name, Submenu);
+        Vue.component(MenuItem.name, MenuItem);
+        Vue.component(MenuItemGroup.name, MenuItemGroup);
+        Vue.component(Container.name, Container);
+        Vue.component(Header.name, Header);
+        Vue.component(Aside.name, Aside);
+        Vue.component(Main.name, Main);
+        Vue.component(Footer.name, Footer);
+        Vue.component(Switch.name, Switch);
+        Vue.component(Table.name, Table);
+        Vue.component(TableColumn.name, TableColumn);
+        Vue.component(Col.name, Col);
         Vue.component(Skeleton.name, Skeleton);
         Vue.component(SkeletonItem.name, SkeletonItem);
         Vue.component(Select.name, Select);
         Vue.component(Option.name, Option);
         Vue.component(Popover.name, Popover);
+        Vue.component(DatePicker.name, DatePicker);
         // Vue.prototype.$message = Message;
     },
 };
