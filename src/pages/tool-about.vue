8ba65d18bbf35b8cfd07fdd7e84e165588d18f00
XuJiaobin <xiaojiuwo57@gmail.com>
2022-08-18 17:05:05 +0800

    更新about

--- src/pages/tool-about.vue
+++ src/pages/tool-about.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h2>关于本站</h2>
-    <p>大家伙！我是徐交彬，本站仅用于练习Vue、Vue-Router、ElementUI等技术，目前尚未兼容各个分辨率推荐使用1600*900以上。</p> 
+    <p>大家伙！我是徐交彬，本站仅用于练习Vue、Vue-Router、ElementUI等技术，目前尚未兼容各个分辨率推荐使用1600*900以上，部分页面细节做的不够好请见谅，可以提issues或直接联系我</p> 
     <p>开源地址：<a
       href="https://github.com/XuJiaobin/practise-tool-Vue"
       target="_blank">https://github.com/XuJiaobin/practise-tool-Vue</a><p>
